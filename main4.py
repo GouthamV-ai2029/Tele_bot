22892b98e1218b04bade583ec0f2a78ce78b7ce1
GouthamV-ai2029 <goutham.v.2029@gmail.com>
2026-03-15 11:36:50 +0530

    Update main4.py

--- main4.py
+++ main4.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from flask import Flask, request
 import telebot
 import os
@@ -6,7 +7,7 @@ TOKEN = "8748531687:AAG8cQiy95YB_lRMxp5AofzcMv6FxV1plxM
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
-
+st.title("TelegramBotHosting")
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Bot running on webhook")
