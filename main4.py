276c443534a8ef1787434da87bff5a121bb2a925
GouthamV-ai2029 <goutham.v.2029@gmail.com>
2026-03-15 11:44:19 +0530

    Update main4.py

--- main4.py
+++ main4.py
@@ -1,28 +1,10 @@
 import streamlit as st
-from flask import Flask, request
 import telebot
-import os
-
-TOKEN = "8748531687:AAG8cQiy95YB_lRMxp5AofzcMv6FxV1plxM
-
-bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 st.title("TelegramBotHosting")
+TOKEN = "8748531687:AAG8cQiy95YB_lRMxp5AofzcMv6FxV1plxM"
+bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Bot running on webhook")
+    bot.send_message(message.chat.id, "Bot is hosting through streamlit")
 
-@app.route(f"/{TOKEN}", methods=["POST"])
-def webhook():
-    json_str = request.get_data().decode("UTF-8")
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "ok", 200
-
-@app.route("/")
-def home():
-    return "Bot Live"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+bot.polling()
